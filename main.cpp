4030df571f5f435fe575029fb86ec3d995065a3b
BowlOfSomething <erzen.jost@gmail.com>
2022-12-29 22:34:06 +0100

    BWL - making the stuff  do the work

--- main.cpp
+++ main.cpp
@@ -1,83 +1,83 @@
 #include <math.h>
+
 #include <fstream>
 #include <iostream>
 
-#include "rapidjson/document.h"
 #include "lattice.h"
-
 
 using namespace std;
 
-
-static char* getCharFromString(const string& str){
-    char *charPtr = new char [str.length()+1];
-    strcpy(charPtr, str.c_str());
-    return charPtr;
+/*static char* getCharFromString(const string& str) {
+	char* charPtr = new char[str.length() + 1];
+	strcpy(charPtr, str.c_str());
+	return charPtr;
 }
 
-static rapidjson::Document getJsonDocumentFromFile(const string& fileName){
-    ifstream fileStream(fileName);
-    if (!fileStream.is_open()) throw runtime_error("Could not open config file");
-    std::string tmp((istreambuf_iterator<char>(fileStream)),
-                     istreambuf_iterator<char>());
-    const char* fileContent = getCharFromString(tmp);
+static rapidjson::Document getJsonDocumentFromFile(const string& fileName) {
+	ifstream fileStream(fileName);
+	if (!fileStream.is_open())
+		throw runtime_error("Could not open config file");
+	std::string tmp((istreambuf_iterator<char>(fileStream)),
+					istreambuf_iterator<char>());
+	const char* fileContent = getCharFromString(tmp);
 
-    rapidjson::Document document;
-    document.Parse<rapidjson::kParseCommentsFlag>(fileContent);
-    delete[] fileContent;
-    return document;
+	rapidjson::Document document;
+	document.Parse<rapidjson::kParseCommentsFlag>(fileContent);
+	delete[] fileContent;
+	return document;
 }
 
-static std::map<std::string, double> getMapFromJsonDocument(rapidjson::Document& document) {
-    std::map<std::string, double> parametersMap;
+static std::map<std::string, double> getMapFromJsonDocument(
+	rapidjson::Document& document) {
+	std::map<std::string, double> parametersMap;
 
-    std::list<std::string> parameters {"rho", "beta", "alpha", "theta", "kappa", "mu", "gamma", "sigma" };
+	std::list<std::string> parameters{"rho",   "beta", "alpha", "theta",
+									  "kappa", "mu",   "gamma", "sigma"};
 
+	for (auto& parameter : parameters) {
+		const char* parameterName = getCharFromString(parameter);
+		parametersMap.emplace(parameter, document[parameterName].GetDouble());
+		delete[] parameterName;
+	}
+	return parametersMap;
+}*/
 
-    for (auto& parameter: parameters) {
-        const char* parameterName = getCharFromString(parameter);
-        parametersMap.emplace(parameter, document[parameterName].GetDouble());
-        delete[] parameterName;
-    }
-    return parametersMap;
-}
+int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		std::cout << "You must enter the json file containing the parameters"
+				  << std::endl;
+		return 1;
+	}
 
-int main(int argc, char* argv[]){
-    if (argc == 1) {
-        std::cout << "You must enter the json file containing the parameters" << std::endl;   
-        return 1;
-    } 
+	bool skipFrames = true;
 
-    bool skipFrames = false;
+	// auto document = getJsonDocumentFromFile(argv[1]);
+	std::map<std::string, double> parameters = {
+		{"size", 600}, {"iterations", 100}, {"frameskips", 0}, {"rho", 0.58},
+		{"beta", 2},   {"alpha", 0.8},		{"theta", 0.011},  {"kappa", 0.1},
+		{"mu", 0.01},  {"gamma", 0.00005},	{"sigma", 0.00005}};
 
-    auto document = getJsonDocumentFromFile(argv[1]);
-    auto parameters = getMapFromJsonDocument(document);
+	size_t iterations = 1000;  // document["iterations"].GetInt();
+	size_t size = 600;
 
+	Lattice lattice(size, parameters);
 
-    size_t iterations = document["iterations"].GetInt();
-    size_t size = document["size"].GetInt();
-    if (document.HasMember("frameskips"))
-        skipFrames = true;
+	for (size_t i = 0; i < iterations; i++) {
+		std::cout << "iterations " + std::to_string(i) + "/" +
+						 std::to_string(iterations)
+				  << "\r";
+		lattice.diffuse();
+		lattice.freeze();
+		lattice.attach();
+		lattice.melt();
+		lattice.addNoise();
 
+		if (skipFrames) {
+			lattice.saveTo("frames/snowflake" + std::string("_") +
+						   std::to_string(i));
+		}
+	}
+	lattice.saveTo("frames/snowflake");
 
-    Lattice lattice(size, parameters);
-
-
-    for (size_t i=0; i<iterations; i++) {
-        std::cout << "iterations " + std::to_string(i) + "/" + std::to_string(iterations) << "\r";
-        lattice.diffuse();
-        lattice.freeze();
-        lattice.attach();
-        lattice.melt();
-        lattice.addNoise();
-
-
-        if (skipFrames) {
-            if (i % (document["frameskips"].GetInt()+1) == 0)
-                lattice.saveTo(document["filepath_prefix"].GetString()+std::string("_")+std::to_string(i));
-        }
-    }
-    lattice.saveTo(document["filepath_prefix"].GetString());
-
-    return 0;
+	return 0;
 }
